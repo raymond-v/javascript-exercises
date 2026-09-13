3c551357b3affeb38613449b1b39dbbf5cc53d8e
Raymond <raymond.vuong@torontomu.ca>
2026-09-13 19:12:41 -0400

    Complete 03-fibonacci

--- foundations/object_basics/03_fibonacci/fibonacci.js
+++ foundations/object_basics/03_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index == 0) {
+        return 0
+    } else if (index < 0) {
+        return 'OOPS'
+    }
+    let next = 1
+    let current = 1
+    for (let i = 2; i < index; i++) {
+        current = current + next
+        next = current - next
+    }
+    return current
 };
 
 // Do not edit below this line
