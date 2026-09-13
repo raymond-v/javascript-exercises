b80ce61a881d48274553fcdd2024ac692a9434a3
Raymond <raymond.vuong@torontomu.ca>
2026-09-13 19:18:48 -0400

    Complete 04-getTheTitles

--- foundations/object_basics/04_getTheTitles/getTheTitles.js
+++ foundations/object_basics/04_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    return array.map(element => {
+        return element["title"]
+    })
 };
 
 // Do not edit below this line
