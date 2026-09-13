8809816ea786a57e1e42a9393c531c516e62e309
Raymond <raymond.vuong@torontomu.ca>
2026-09-13 17:28:08 -0400

    Complete 01_calculator

--- foundations/object_basics/01_calculator/calculator.js
+++ foundations/object_basics/01_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => {
+    return total + current
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => {
+    return total * current
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let sum = 1
+	while (num) {
+    sum *= num
+    num--
+  }
+  return sum
 };
 
 // Do not edit below this line
