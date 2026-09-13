5ea33cfb70fe9ef398bfb38f18f1c460956e6db4
Raymond <raymond.vuong@torontomu.ca>
2026-09-13 17:48:35 -0400

    Complete 02-palindromes

--- foundations/object_basics/02_palindromes/palindromes.js
+++ foundations/object_basics/02_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[^a-z0-9]/g, '')
+    let reverse = ''
+    let normal = string.split('')
+    for (let i = normal.length - 1; i >= 0; i--) {
+        reverse += normal[i]
+    }
+    return string === reverse
 };
 
 // Do not edit below this line
