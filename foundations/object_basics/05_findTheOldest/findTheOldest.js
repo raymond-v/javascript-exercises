7c9bb8ee247cc517433a7f9ab00aebd1da740c1f
Raymond <raymond.vuong@torontomu.ca>
2026-09-13 19:53:52 -0400

    Complete 05-findTheOldest

--- foundations/object_basics/05_findTheOldest/findTheOldest.js
+++ foundations/object_basics/05_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const person = array.reduce((biggest, current) => {
+        if (((current.yearOfDeath ?? new Date().getFullYear()) - current.yearOfBirth) > ((biggest.yearOfDeath ?? new Date().getFullYear()) - biggest.yearOfBirth)) {
+            return current
+        } else {
+            return biggest
+        }
+    })
+    return person
 };
 
 // Do not edit below this line
